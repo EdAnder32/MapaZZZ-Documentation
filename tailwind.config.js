af5828d9cc379a95959b7541c2c3d3ae52ed37ac
Edmilson Alexandre <145597126+EdAnder32@users.noreply.github.com>
2025-07-06 21:36:38 +0100

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}',
+    './docs/**/*.{md,mdx}',
+    './blog/**/*.{md,mdx}',
+    './src/pages/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
